47ade53964eaa0b3ced1d263059c959386acf93d
GS-Moonlyn <gsmolive@gmail.com>
2021-09-05 19:10:40 -0300

    RigidBody e Massa Randomizada

---
+++ Assets/cubeGravity.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeGravity : MonoBehaviour
+{
+
+    int minMass = 1;
+    int maxMass = 8;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().mass = Random.Range(minMass, maxMass);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
