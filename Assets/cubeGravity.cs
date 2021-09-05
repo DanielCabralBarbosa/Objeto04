8147aa8b0f9f304a32ce17df58340b389d531077
GS-Moonlyn <gsmolive@gmail.com>
2021-09-05 19:31:03 -0300

    Variaveis de gravidade incompletas

--- Assets/cubeGravity.cs
+++ Assets/cubeGravity.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class cubeGravity : MonoBehaviour
 {
 
-    int minMass = 1;
-    int maxMass = 8;
-
+    public int minMass = 0;
+    public int maxMass = 1;
+    public int minSpeed = 1;
+    public int maxSpeed = 2;
+    int speed;
+    int force = 1;
+  
 
     // Start is called before the first frame update
     void Start()
     {
         this.GetComponent<Rigidbody>().mass = Random.Range(minMass, maxMass);
+        this.speed = Random.Range(minSpeed, maxSpeed);
     }
 
     // Update is called once per frame
